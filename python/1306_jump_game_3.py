56f7dc08f8e002f66c1659f18086ac3161db366a
Cotang01 <127985804+Cotang01@users.noreply.github.com>
2023-11-26 02:52:14 +0500

    1306 rm unused import

--- python/1306_jump_game_3.py
+++ python/1306_jump_game_3.py
@@ -3,7 +3,6 @@ Given an array of non-negative integers arr, you are initially positioned at
 start index of the array. When you are at index i, you can jump to i + arr[i]
 or i - arr[i], check if you can reach any index with value 0.
 """
-import math
 
 
 class Solution:
